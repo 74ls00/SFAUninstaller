93adb46e97b9ba85a4a06f9007385121dfd94f37
74ls00 <userled@yandex.ru>
2018-09-06 16:47:25 +0300

    .

--- sfakiler.bat
+++ sfakiler.bat
@@ -1,18 +1,16 @@
 @echo off
-chcp 866
 rem for x64
-rem Запускать из проводника!
-
 
 taskkill /im SFAUpdater.exe /t /f
 taskkill /im sfa.exe /t /f
-
 rd /s /q "%ProgramFiles(x86)%\Smart File Advisor"
 rd /s /q "%ProgramData%\Microsoft\Windows\Start Menu\Programs\Smart File Advisor"
 
 reg delete "HKLM\SOFTWARE\Microsoft\Shared Tools\MSConfig\startupreg\Smart File Advisor" /f
 reg delete "HKLM\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Run" /v SFAUpdater /f
 reg delete "HKLM\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Run" /v "Smart File Advisor" /f
+reg delete "HKLM\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Smart File Advisor_is1" /f
+reg delete "HKLM\SOFTWARE\Wow6432Node\Smart File Advisor" /f
 
 reg delete HKCR\*\shell\!sfa /f
 reg delete HKCR\*\shell\sfa_checksum /f
@@ -20,26 +18,22 @@ reg delete "HKCU\Software\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Com
 
 reg delete HKCR\Unknown\shell\openas\command /v sfa_backup /f
 reg delete HKCR\Unknown\shell\openas\command /v sfa_backup_DelegateExecute /f
-
 reg add HKCR\Unknown\shell\openas\command /ve /t REG_EXPAND_SZ /d "%%SystemRoot%%\system32\rundll32.exe %%SystemRoot%%\system32\shell32.dll,OpenAs_RunDLL %%1" /f
-rem reg add HKCR\Unknown\shell\openas\command /ve /t REG_EXPAND_SZ /d ^%SystemRoot^%\system32\rundll32.exe" "^%SystemRoot^%\system32\shell32.dll,OpenAs_RunDLL" "%1 /f
-
 reg add HKCR\Unknown\shell\openas\command /v DelegateExecute /t REG_SZ /d "{e44e9428-bdbc-4987-a099-40dc8fd255e7}" /f
 
 reg delete HKCR\Unknown\shell\opendlg\command /v sfa_backup /f
 reg add HKCR\Unknown\shell\opendlg\command /ve /t REG_EXPAND_SZ /d "%%SystemRoot%%\system32\rundll32.exe %%SystemRoot%%\system32\shell32.dll,OpenAs_RunDLL %%1" /f
-rem reg add HKCR\Unknown\shell\opendlg\command /ve /t REG_EXPAND_SZ /d ^%SystemRoot^%\system32\rundll32.exe" "^%SystemRoot^%\system32\shell32.dll,OpenAs_RunDLL" "%1 /f
 
-reg delete "HKLM\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Smart File Advisor_is1" /f
 
-reg delete "HKLM\SOFTWARE\Wow6432Node\Smart File Advisor" /f
+
 
 
 
 rem HKLM\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall
 
 
-
+rem reg add HKCR\Unknown\shell\openas\command /ve /t REG_EXPAND_SZ /d ^%SystemRoot^%\system32\rundll32.exe" "^%SystemRoot^%\system32\shell32.dll,OpenAs_RunDLL" "%1 /f
+rem reg add HKCR\Unknown\shell\opendlg\command /ve /t REG_EXPAND_SZ /d ^%SystemRoot^%\system32\rundll32.exe" "^%SystemRoot^%\system32\shell32.dll,OpenAs_RunDLL" "%1 /f
 
 pause
 
